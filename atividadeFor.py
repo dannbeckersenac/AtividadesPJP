467cfbc0aabd2de1fa5b72e706a925cdd5311430
dannbeckersenac <daniel.bortoluzzi@prof.sc.senac.br>
2026-05-06 21:50:43 -0300

    Atividade for

---
+++ atividadeFor.py
@@ -0,0 +1,24 @@
+# Salva na memória uma variável nome frutas, inserindo uma lista de frutas
+frutas = ["manga", "banana", "abacaxi", "laranja", "uva"]
+
+# Entrada do usuário pedindo para digitar nome da fruta favorita
+fruta_favorita = input("Qual sua fruta favorita?: ")
+
+# SE a fruta favorita NÃO ESTÁ NA lista frutas
+if fruta_favorita not in frutas:
+    # Faça isso (exibir mensagem e sai do sistema):
+    print("Sua fruta favorita não está na lista!")
+    exit()
+
+# PARA cada posição (índice) e fruta NA lista numerada de frutas
+for posicao, fruta in enumerate(frutas):
+    # Faça isso:
+    # SE a fruta dessa iteração é igual a fruta favorita
+    if fruta == fruta_favorita:
+        # Salva numa nova variável a posição dessa iteração
+        posicao_fruta_favorita = posicao
+        # Quebra o for (faz ele parar)
+        break 
+
+# Saída do algoritmo (print)
+print(f"Minha fruta favorita está na posição índice {posicao_fruta_favorita}")
