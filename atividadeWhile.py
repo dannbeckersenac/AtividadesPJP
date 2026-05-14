4d260bf626d9a40a1a455ad4f1e66f2325117c4f
dannbeckersenac <daniel.bortoluzzi@prof.sc.senac.br>
2026-05-13 21:22:47 -0300

    envio atividade while

---
+++ atividadeWhile.py
@@ -0,0 +1,20 @@
+import random
+
+numero_secreto = random.randint(1, 10)
+palpite = 0
+tentativas = 0
+
+while palpite != numero_secreto:
+    palpite = int(input("Adivinhe o número (1 a 10): "))
+    if palpite != numero_secreto:
+        print("Errou! Tente novamente.")
+
+    tentativas = tentativas + 1
+
+
+if tentativas <= 1:
+    print(f"Acertou! Parabéns! Você tentou {tentativas} vez")
+else:
+    print(f"Acertou! Parabéns! Você tentou {tentativas} vezes")
+
+    
